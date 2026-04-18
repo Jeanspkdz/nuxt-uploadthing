0387e61426eeebe73bcd91cf6affde5abaf315d9
jeanspkdz <jeanchua1304@gmail.com>
2026-04-18 17:04:40 -0500

    chore: initialize Nuxt module template scaffold

---
+++ playground/app.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const { isUploading, routeConfig, startUpload } = jp_useUploadThing('imageUploader')
+</script>
+
+<template>
+  <div>
+    <!-- <UploadThingUploadButton
+      :config="{
+        endpoint: 'imageUploader',
+        onClientUploadComplete: (res) => {
+          console.log('uploaded', res)
+        },
+        onUploadError: (err) => {
+          console.error(err)
+        },
+      }"
+    /> -->
+
+    <!-- <UploadThingUploadDropzone
+      :config="{
+        endpoint: 'imageUploader',
+        onClientUploadComplete: (res) => {
+          console.log('uploaded', res)
+        },
+        onUploadError: (err) => {
+          console.error(err)
+        },
+      }"
+    /> -->
+
+    <UploadButtonTest />
+    Jeans
+  </div>
+</template>
---
+++ playground/nuxt.config.ts
@@ -0,0 +1,8 @@
+export default defineNuxtConfig({
+  modules: ['nuxt-uploadthing'],
+  devtools: { enabled: true },
+  compatibilityDate: 'latest',
+  uploadthing: {
+
+  }
+})
